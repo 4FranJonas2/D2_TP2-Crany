c506394938dde95e32cd444a78ffdb493cfaddac
Francisco <franjonas98@hotmail.com>
2026-05-15 21:00:46 -0300

    Add Menus basic change scene scripts

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenRules()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Salir del juego");
+    }
+}
